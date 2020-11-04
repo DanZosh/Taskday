a4a5065cf47432766f7e3512496d262d7fc2be35
Danny Maier <dannymaier@gmail.com>
2020-11-04 12:52:01 -0800

    the functionality of the click event is working. To do next is get the localstorage to function properly. Right now i have the psuedo index to be abel to make a comaprison and correlate the textarea content wiht the timeArray

--- script.js
+++ script.js
@@ -5,40 +5,59 @@ var currentTimeEl = $('#currentTime');
     console.log(currentTimeEl)
 var timeblockEl = $('.timeblock')
     console.log(timeblockEl)
+
 // Assemble: Create/select global variables 
 var timeArray=[
     hour1={
         time:9,
-        hour:9},
+        hour:9,
+        content:"Add Task",
+    },
     hour2={
         time:10,
-        hour:10},
+        hour:10,
+        content:"",
+    },
     hour3={
         time:11,
-        hour:11},
+        hour:11,
+        content:"",
+    },
     hour4={
         time:12,
-        hour:12},
+        hour:12,
+        content:"",
+    },
     hour5={
         time:1,
-        hour:13},
+        hour:13,
+        content:"",
+    },
     hour6={
         time:2,
-        hour:14},
+        hour:14,
+        content:"",
+    },
     hour7={
         time:3,
-        hour:15},
+        hour:15,
+        content:"",
+    },
     hour8={
         time:4,
-        hour:16},
+        hour:16,
+        content:"",
+    },
     hour9={
         time:5,
-        hour:17},
+        hour:17,
+        content:"",
+    },
         ]
     console.log(timeArray)
 // toggle between the two lines below to test
 var currentHour = moment().hour()
-// var currentHour = 17
+// var currentHour = 11
     console.log(currentHour)
 
 //DISPLAY the current Day and Time
@@ -52,7 +71,18 @@ setInterval(function(){
     return    
     },1000);
 
-//the guts of my for loop are here:
+init();
+//CHECK if there are already items stored in local storage.
+function init(){
+    var storedTimeArray = localStorage.getItem(timeArray)
+    if (storedTimeArray){
+        timeArray=JSON.parse(storedTimeArray)
+    }
+    renderTimeArray();
+}
+//CREATE the line items and add them to the page:
+//LOOP over the `timeArray` to create a line for each time object
+function renderTimeArray(){
 for (let i = 0; i < timeArray.length; i++) {
     var hourX = timeArray[i];
         console.log(hourX)
@@ -60,12 +90,13 @@ for (let i = 0; i < timeArray.length; i++) {
         console.log(hourXTime)
     var hourXhour = hourX.hour
         console.log(hourXhour)
+    var hourXContent = hourX.content
+        console.log(hourXContent)
 
-
-
-
+//FOR each time object CREATE the components of the line items, ADD attributes and APPEND to each other and the page
     var trEl = $("<tr>")
-        trEl.prop('id', "trEl")
+        trEl.prop('id', "trEl").data("hour",`${hourXhour}`).prop("data-hour",`${hourXhour}`).addClass(`${hourXhour}`)
+        //in the above, only class appears to be appending to the element.
     var thEldiv = $("<div>")
         thEldiv.prop('id', "thEldiv")
     var thEl = $("<th>")
@@ -74,6 +105,8 @@ for (let i = 0; i < timeArray.length; i++) {
         tdEl1.prop('id', "tdEl1")
     var textarea =  $("<textarea>")
         textarea.addClass("form-control").prop("id", "textArea").prop("rows", "1")
+        textarea.append(`${hourXContent}`)
+        //COMPARE the current hour to the time object `textarea` and ADD the color coding class based on that comparison
         if(hourXhour===currentHour){
             textarea.addClass("present");
         }else if (hourXhour < currentHour){
@@ -95,13 +128,7 @@ for (let i = 0; i < timeArray.length; i++) {
     trEl.append(tdEl2)
     timeblockEl.append(trEl)
 
-    // timeblockEl.append(trEl)
-    // timeblockEl.append(thEl)
-    // tdEl1.append(textarea)
-    // timeblockEl.append(tdEl1)
-    // tdEl2.append(button)
-    // timeblockEl.append(tdEl2)
-
+//TEST your assignment are what you expect. Comment out but keep them when you're done just in case.
     // console.log(trEl)
     // console.log(thEl)
     // console.log(tdEl1)
@@ -109,3 +136,40 @@ for (let i = 0; i < timeArray.length; i++) {
     // console.log(tdEl2)
     // console.log(button)
 }
+}
+
+//ADD a click event to the button that can be heard by the parent element
+
+// SAVE the textArea objects:
+function storeTimeArray(event){
+    event.preventDefault();
+    console.log(this)
+//starting from the click event, traverse up to the parent element, the trEl
+    var lineItem = this.closest("tr")
+        console.log(lineItem)
+//from the trEl, get the content of the textArea:
+    var taskItem = lineItem.childNodes[1].childNodes[0].value
+        console.log(taskItem)
+//get the index in he div class to be used as the item key
+    var psuedoIndex = lineItem.childNodes[0].childNodes[0].getAttribute('id')
+        console.log(psuedoIndex)
+//SET the content of the textArea to the value of key `content`in `timeArray` for that hour
+    // timeArray.index.content=taskItem
+
+//SET timeArray to local storage
+    localStorage.setItem("timeArray", JSON.stringify(timeArray))
+
+
+
+
+    
+
+    
+
+}
+//ADD click event to the container holding the class:timeblock element and TARGET the element clicked for which the id = `button`.
+timeblockEl.on("click","#button", storeTimeArray);
+
+// on a click event, get the content of the textArea and add it to the corresponding object.
+
+
